9f2633aca49b906ccba2483c547e0fbbab2edf59
Michał Kaczmarek <michal@MacBook-Pro-Micha.local>
2022-10-27 13:01:58 +0200

    Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,50 @@
 import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Link from "next/link";
 
 type Props = {}
 
 export default function Hero({}: Props) {
+    const [text, count] = useTypewriter({
+        words: [
+            "Hi, The Name's Michal",
+            "Guy-who-loves-Coffee.tsx",
+            "<ButLovesToCodeMore />",
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    });
     return (
-        <div>
+        <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+            <BackgroundCircles />
+            <img
+                className="relative rounded-full h-32 w-32 object-cover"
+                src="https://png.pngtree.com/png-clipart/20211116/original/pngtree-halloween-scary-party-flyer-template-for-png-image_6927855.png"
+                alt=""
+            />
+            <div className="z-20">
+                <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[10px]">Junior Software Developer</h2>
+                <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+                    <span className="mr-3">{text}</span>
+                    <Cursor cursorColor="#F7AB0A" />
+                </h1>
 
+                <div className="pt-5">
+                    <Link href="#about">
+                        <button className="heroButton">About</button>
+                    </Link>
+                    <Link href="#experience">
+                        <button className="heroButton">Experience</button>
+                    </Link>
+                    <Link href="#skills">
+                        <button className="heroButton">Skills</button>
+                    </Link>
+                    <Link href="#projects">
+                        <button className="heroButton">Projects</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,15 @@ import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
-        <title>Create Next App</title>
+        <title>Michal Portfolio</title>
       </Head>
 
         <Header />
 
         {/* Hero */}
-        <section id="hero">
+        <section id="hero" className="snap-center">
             <Hero />
         </section>
 
