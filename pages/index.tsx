b5e8da49e0d7f0b65e1168618aa03fc4f3402940
Michał Kaczmarek <michal@MacBook-Pro-Micha.local>
2022-10-27 21:54:09 +0200

    Contact section

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,11 @@ import About from "../components/About";
 import WorkExperience from "../components/WorkExperience";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>Michal Portfolio</title>
       </Head>
@@ -34,7 +35,9 @@ export default function Home() {
             <Projects />
         </section>
 
-        {/* Contact Me */}
+        <section id="contact" className="snap-center">
+            <Contact />
+        </section>
     </div>
   );
 };
